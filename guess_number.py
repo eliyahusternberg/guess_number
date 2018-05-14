5c7eaad23684d3dd2a6667ccf6d309bdf631d91b
eliyahusternberg <33271054+eliyahusternberg@users.noreply.github.com>
2018-05-13 20:24:10 -0400

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -9,7 +9,7 @@ the_number = random.randint(0, 100)
 guess =-1
 name = input('Player what is your name? ')
 while guess != the_number:
-    guess_text = input('Guess {} a number between 0 and 100:'.format(name))
+    guess_text = input('{} guess a number between 0 and 100:'.format(name))
     guess = int(guess_text)
 
     if guess < the_number:
